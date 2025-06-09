053312d61e19deb642f1c75ef2c4e664664f0e4c
codingMaz <m.balandari.toroghi@gmail.com>
2025-06-09 07:13:27 +0330

    add model to admin and edit it

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "body",
+        "author",
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
